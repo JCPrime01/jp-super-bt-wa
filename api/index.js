dbdefb05b674dcd68de610ff81b5cf222db76888
JCPrime01 <vanzokiller@gmail.com>
2026-06-29 16:56:40 -0300

    chore: shareCode V8VTKRXQ91D -> 72AAJ7SMKDC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/psawehc2?shareCode=V8VTKRXQ91D&afp=boost&home=1' });
+    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/psawehc2?shareCode=72AAJ7SMKDC&afp=boost&home=1' });
     res.end();
   }
