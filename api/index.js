454b69bc897358dd6af01c37c2090454815566d3
JCPrime01 <vanzokiller@gmail.com>
2026-06-30 19:03:59 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,8 +22,26 @@
       res.writeHead(302, { Location: 'https://grupojogadorcaro.com.br/quem-e-jota' });
       res.end();
       return;
+       }
+    
+    const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
+
+    const shareCode = '&shareCode=QY3MR187GKA'
+    
+    const afp = '&afp=wa'
+    
+    const source = '&home=1'
+
+    const destino = new URL(url + shareCode + afp + source)
+
+    const entrada = new URL(req.url, `https://${req.headers.host}`).searchParams;
+
+    for (const [chave, valor] of entrada) {
+      if (chave.startsWith('utm_') || chave === 'source_id' || chave === 'fbclid' || chave === 'gclid') {
+        destino.searchParams.set(chave, valor);
+      }
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.bateu.bet.br/psawehc2?shareCode=9D0UZM88F9S&afp=boost&home=1' });
+    res.writeHead(302, { Location: destino.toString() });
     res.end();
   }
