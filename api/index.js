3fa621c607676f12a16e365585247acf62b48ff6
JCPrime01 <vanzokiller@gmail.com>
2026-07-01 10:00:10 -0300

    chore: shareCode QY3MR187GKA -> U6US8R3B935 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=QY3MR187GKA'
+    const shareCode = '&shareCode=U6US8R3B935'
     
     const afp = '&afp=boost'
 
