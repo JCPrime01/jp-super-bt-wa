894a4f77bd3f1343be29270a49d89b72103ed519
JCPrime01 <vanzokiller@gmail.com>
2026-07-01 13:35:14 -0300

    chore: shareCode T5B4UU4018H -> 7H3UUII72WB (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=T5B4UU4018H'
+    const shareCode = '&shareCode=7H3UUII72WB'
     
     const afp = '&afp=boost'
 
