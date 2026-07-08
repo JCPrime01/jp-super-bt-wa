3411a71c5218dd0d5bb5950ed2438ce01442a9d1
JCPrime01 <vanzokiller@gmail.com>
2026-07-08 17:33:13 -0300

    chore: shareCode HLY2NU8PFPZ -> SE6UU1QMQXS (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=HLY2NU8PFPZ'
+    const shareCode = '&shareCode=SE6UU1QMQXS'
     
     const afp = '&afp=boost'
 
