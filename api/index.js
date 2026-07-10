02b11edb24c5e312d1691ff48d0da11077112c1e
JCPrime01 <vanzokiller@gmail.com>
2026-07-10 16:36:26 -0300

    chore: shareCode QBBULN5E869 -> JCIGQYFIJC1 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=QBBULN5E869'
+    const shareCode = '&shareCode=JCIGQYFIJC1'
     
     const afp = '&afp=boost'
 
