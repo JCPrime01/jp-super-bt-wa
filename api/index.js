3327d724d32001e021d00bcae567f75a5539d73c
JCPrime01 <vanzokiller@gmail.com>
2026-07-11 11:21:19 -0300

    chore: shareCode VZ6229LQ57N -> UJD06JZZZJX (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=VZ6229LQ57N'
+    const shareCode = '&shareCode=UJD06JZZZJX'
     
     const afp = '&afp=boost'
 
