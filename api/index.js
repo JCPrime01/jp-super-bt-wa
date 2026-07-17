ed121e63f7b893bbaec9042a3dacc4c276577ccd
JCPrime01 <vanzokiller@gmail.com>
2026-07-17 14:58:37 -0300

    chore: shareCode 60C0L0X87HE -> H2FH05DMZHJ (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=60C0L0X87HE'
+    const shareCode = '&shareCode=H2FH05DMZHJ'
     
     const afp = '&afp=boost'
 
