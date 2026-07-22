689e49a00d4e671e54e010624bca0c1ea46c663c
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 10:25:53 -0300

    chore: shareCode HP8YPL2JED7 -> 39CX6R59ARD (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=HP8YPL2JED7'
+    const shareCode = '&shareCode=39CX6R59ARD'
     
     const afp = '&afp=boost'
 
