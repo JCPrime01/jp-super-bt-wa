57360758e4e95a5fad54cb3cbba64cea6ebc0c10
JCPrime01 <vanzokiller@gmail.com>
2026-07-26 10:24:14 -0300

    chore: shareCode 8OICCMORVDC -> OW0IQMI1SXH (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=8OICCMORVDC'
+    const shareCode = '&shareCode=OW0IQMI1SXH'
     
     const afp = '&afp=boost'
 
