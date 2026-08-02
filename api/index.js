793e7b0551acbdd11412ab129c615f2c45200c93
JCPrime01 <vanzokiller@gmail.com>
2026-08-02 11:10:34 -0300

    chore: shareCode OYJJ74X2FFM -> TUG01XI3GOC (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=OYJJ74X2FFM'
+    const shareCode = '&shareCode=TUG01XI3GOC'
     
     const afp = '&afp=boost'
 
