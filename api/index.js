b8325f4f724251adf042d53699bab6f6c8a5f626
JCPrime01 <vanzokiller@gmail.com>
2026-08-03 10:34:39 -0300

    chore: shareCode TUG01XI3GOC -> XWE9WO1UESM (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=TUG01XI3GOC'
+    const shareCode = '&shareCode=XWE9WO1UESM'
     
     const afp = '&afp=boost'
 
