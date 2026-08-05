2ee1ca727b3c18431310c8bc030d23e9e4f096d1
JCPrime01 <vanzokiller@gmail.com>
2026-08-05 09:53:42 -0300

    chore: shareCode PJU17HMAHXE -> 53HAPPY1LQ9 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=PJU17HMAHXE'
+    const shareCode = '&shareCode=53HAPPY1LQ9'
     
     const afp = '&afp=boost'
 
