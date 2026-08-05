8fab8af49b551d6e4968254648c1a4fda45b3926
JCPrime01 <vanzokiller@gmail.com>
2026-08-05 16:51:11 -0300

    chore: shareCode 53HAPPY1LQ9 -> VIDU5L8D7Y8 (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=53HAPPY1LQ9'
+    const shareCode = '&shareCode=VIDU5L8D7Y8'
     
     const afp = '&afp=boost'
 
