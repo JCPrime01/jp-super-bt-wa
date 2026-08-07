68ea085a8b3ac4b67fc762c36cf406c8672b4ca5
JCPrime01 <vanzokiller@gmail.com>
2026-08-07 10:02:54 -0300

    chore: shareCode EVINFF86LHI -> JP4AUWL5DWD (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
     
     const url = 'https://go.aff.bateu.bet.br/l9da18p1?campaign_id=31558'
 
-    const shareCode = '&shareCode=EVINFF86LHI'
+    const shareCode = '&shareCode=JP4AUWL5DWD'
     
     const afp = '&afp=boost'
 
